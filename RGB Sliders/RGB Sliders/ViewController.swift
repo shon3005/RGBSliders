2149cf2fdcc3277d012bdb2e43e70fba287dbead
Shaun Chua <shon3005@gmail.com>
2015-05-29 13:31:25 +0800

    first commit

---
+++ RGB Sliders/RGB Sliders/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Shaun Chua on 29/5/15.
+//  Copyright (c) 2015 Shaun Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+
+}
+
