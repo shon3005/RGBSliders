b1ebc31d7e00162435374d62c6dd8f0c66bc516f
Shaun Chua <shon3005@gmail.com>
2015-05-30 01:46:42 +0800

    first commit

---
+++ PhotoFilters/PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Shaun Chua on 30/5/15.
+//  Copyright (c) 2015 Shaun Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
